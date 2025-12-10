a0a2da344543fd3257887f5b01b9f2a5cc5dee05
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-10 14:18:30 +1100

    Show error page if folder path doesn't exist

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,13 +1,73 @@
-const getFilesPage = (req, res) => {
+import { prisma } from "../lib/prisma.js";
+
+const getFilesPage = async (req, res) => {
   if (res.locals.currentUser) {
-    res.render("index", {
-      title: "Nimbus | Files",
-      page: "pages/files",
-      path: req.path,
-    });
+    const folderId = await getFoldersId(res.locals.currentUser.id, req.path);
+    if (folderId) {
+      res.render("index", {
+        title: "Nimbus | Files",
+        page: "pages/files",
+        path: req.path,
+      });
+    } else {
+      res.status(404).render("index", {
+        title: "Nimbus",
+        page: "pages/folder_error",
+      });
+    }
   } else {
     res.redirect("/login");
   }
+};
+
+const getFoldersId = async (userId, folderPath) => {
+  let rootFolderId;
+  try {
+    const results = await prisma.file.findMany({
+      where: {
+        user_id: userId,
+        parent_id: null,
+        type: "FOLDER",
+      },
+      select: {
+        id: true,
+      },
+    });
+    rootFolderId = results[0].id;
+  } catch {
+    console.error("Failed to find user's root folder");
+    return null;
+  }
+
+  // Path starts with '/' so first element is empty
+  const folders = folderPath.split("/").slice(1);
+  let currentFolderId = rootFolderId;
+  for (const folder of folders) {
+    if (folder.length > 0) {
+      let results;
+      try {
+        results = await prisma.file.findMany({
+          where: {
+            user_id: userId,
+            parent_id: currentFolderId,
+            name: folder,
+            type: "FOLDER",
+          },
+        });
+      } catch {
+        console.error("Failed to check if folder exists");
+        return null;
+      }
+
+      if (results.length === 1) {
+        currentFolderId = results[0].id;
+      } else {
+        return null;
+      }
+    }
+  }
+
+  return currentFolderId;
 };
 
 export { getFilesPage };
