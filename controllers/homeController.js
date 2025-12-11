fff232fabe1226fb8e153f768519f6275b46fe90
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-11 23:00:01 +1100

    Add recent files to home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,32 @@
-const getHomePage = (req, res) => {
+import { prisma } from "../lib/prisma.js";
+
+const getHomePage = async (req, res) => {
   if (res.locals.currentUser) {
-    res.render("index", { title: "Nimbus", page: "pages/home" });
+    const userId = Number(res.locals.currentUser.id);
+    const files = await getMostRecentFiles(userId);
+    res.render("index", { title: "Nimbus", page: "pages/home", files: files });
   } else {
     res.redirect("/login");
   }
+};
+
+const getMostRecentFiles = async (userId) => {
+  let results;
+  try {
+    results = await prisma.file.findMany({
+      where: {
+        user_id: userId,
+        type: "FILE",
+      },
+      take: 10,
+      orderBy: {
+        modified_on: "desc",
+      },
+    });
+  } catch {
+    console.error(`Failed to get most recent files for user ID - ${userId}`);
+  }
+  return results;
 };
 
 export { getHomePage };
