2cc7bec3d64daa3f3960880f482bfeffd6435e9c
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-11 21:53:57 +1100

    Uploading of same name files will update the existing record

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,17 +1,46 @@
 import { prisma } from "../lib/prisma.js";
 
 const uploadFile = async (req, res) => {
-  // CKYTODO: If file name already exists in current folder do an update instead
-  await prisma.file.create({
-    data: {
-      name: req.file.originalname,
+  const filename = req.file.originalname;
+  const userId = res.locals.currentUser.id;
+  const parentFolderId = Number(req.body["parent-folder-id"]);
+  const fileUrl = req.file.path;
+  const size = req.file.size;
+
+  const existingFile = await prisma.file.findFirst({
+    where: {
+      name: filename,
+      user_id: userId,
+      parent_id: parentFolderId,
       type: "FILE",
-      user_id: res.locals.currentUser.id,
-      parent_id: Number(req.body["parent-folder-id"]),
-      size: req.file.size,
-      url: req.file.path,
     },
   });
+
+  if (existingFile) {
+    await prisma.file.update({
+      data: {
+        id: existingFile.id,
+        url: fileUrl,
+        size: size,
+        modified_on: new Date(),
+      },
+      where: {
+        id: existingFile.id,
+      },
+    });
+  } else {
+    await prisma.file.create({
+      data: {
+        name: req.file.originalname,
+        type: "FILE",
+        user_id: res.locals.currentUser.id,
+        parent_id: Number(req.body["parent-folder-id"]),
+        size: size,
+        url: fileUrl,
+      },
+    });
+  }
+
   res.json({ success: true });
 };
 
