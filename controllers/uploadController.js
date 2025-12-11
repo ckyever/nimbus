1dd05008f8365864e0b100d40aa9b12d7affa0e9
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-11 15:41:40 +1100

    Add logic to save uploaded file to database

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,5 +1,17 @@
-const uploadFile = (req, res) => {
-  console.log(req.file.path);
+import { prisma } from "../lib/prisma.js";
+
+const uploadFile = async (req, res) => {
+  // CKYTODO: If file name already exists in current folder do an update instead
+  await prisma.file.create({
+    data: {
+      name: req.file.originalname,
+      type: "FILE",
+      user_id: res.locals.currentUser.id,
+      parent_id: Number(req.body["parent-folder-id"]),
+      size: req.file.size,
+      url: req.file.path,
+    },
+  });
   res.json({ success: true });
 };
 
